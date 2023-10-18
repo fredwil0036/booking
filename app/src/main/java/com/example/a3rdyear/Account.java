8846f761682aff82cc0b081ff7bbd2597491c2ff
Fredwil Membrere <fredwilmembrere@gmail.com>
2023-10-18 19:27:42 +0700

    finish project

---
+++ app/src/main/java/com/example/a3rdyear/Account.java
@@ -0,0 +1,24 @@
+package com.example.a3rdyear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+
+
+    }
+}
